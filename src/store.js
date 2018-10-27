ef7ef85a3b988646dd18af2bc2b4e715274c4a7b
Sim Gill <simgill89@gmail.com>
2018-10-27 00:30:38 -0700

    s

--- src/store.js
+++ src/store.js
@@ -1,33 +1,15 @@
-// import {createStore, combineReducers, applyMiddleware}  from "redux";
-// import { createLogger } from 'redux-logger'
-// import thunk                                            from "redux-thunk";
-// // import promise                                          from "redux-promise-middleware";
-// import firstReducer                                     from "./reducers/firstReducer"
-// import secondReducer                                    from "./reducers/secondReducer"
-
-
-// export default createStore(
-//     combineReducers({
-        
-//         firstReducer:firstReducer,
-//         secondReducer:secondReducer
-
-//     }),
-//     {},
-//     applyMiddleware(createLogger(), thunk)
-// );
-
 
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
-
 import firstReducer from "./reducers/firstReducer";
 import secondReducer from "./reducers/secondReducer";
 
+
 if (process.env.NODE_ENV !== 'production') {
        console.log('Looks like we are in development mode!');
-     }
+}
+
 
 const logger = createLogger();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const path = require('path');
 
 
 
 
 let config = {
+
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -35,7 +38,8 @@ let config = {
     new HtmlWebPackPlugin({
       template: "./public/index.html",
       filename: "./index.html"
-    })
+    }),
+    new CleanWebpackPlugin(['dist']),
   ]
 };
 
@@ -45,12 +49,15 @@ module.exports = (env, argv) => {
     console.log('argv====>',argv.mode)
       if (argv.mode === 'development') {
         config.devtool = 'source-map';
+        config.mode = argv.mode
       }
     
       if (argv.mode === 'production') {
-        //...
+        config.mode = argv.mode
         return config
       }
+
+      console.log('hit')
     
       return config;
  };
