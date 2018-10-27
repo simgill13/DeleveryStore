1753b28f4af56492e1fdeb5e4a63dfa94b8f4f92
Sim Gill <simgill89@gmail.com>
2018-10-27 12:11:34 -0700

    s

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+
+module.exports = {
+  app: function () {
+    const app = express()
+    const indexPath = path.join(__dirname, './dist/index.html')
+    const publicPath = express.static(path.join(__dirname, './dist/App.bundle.js'))
+    console.log('+++++',indexPath)
+    console.log('piblic',path.join(__dirname, './dist/App.bundle.js'))
+
+    app.use('/public', publicPath)
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+    return app
+  }
+}
